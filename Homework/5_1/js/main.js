a6fb908a83702f558be341d1606d7e4e494bb05e
X <sersergiusx@gmail.com>
2020-12-10 02:08:36 +0200

    5.1 fix

--- Homework/5_1/js/main.js
+++ Homework/5_1/js/main.js
@@ -17,16 +17,27 @@ let arr2 = [];
 
 for (let key in obj1) {
   if (Array.isArray(obj1[key])) {
-    arr1.push(obj1[key]);
+    for (let i=0; i<obj1[key].length; i++) {
+      arr1.push(obj1[key][i]);
+    }
   }
 }
 
 for (let key in obj2) {
   if (Array.isArray(obj2[key])) {
-    arr2.push(obj2[key]);
+    for (let i=0; i<obj2[key].length; i++) {
+      arr2.push(obj2[key][i]);
+    }
   }
 }
 
-let resultArray = arr1.concat(arr2);
+let allArray = arr1.concat(arr2);
+let result = 0;
 
-console.log(resultArray);
+for (let i=0; i<allArray.length; i++) {
+  if (typeof(allArray[i]) == 'number') {
+    result += allArray[i];
+  }
+}
+
+console.log(result);
